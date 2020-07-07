0b1957e26bd52e6807090389431715f9e2aa1570
JRPino <pino3789@gmail.com>
2020-07-07 11:57:45 -0700

    Added links to html, added code for SavedBook, SearchBar, Saved.js, and Toast.js with css

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
 import {BrowserRouter as Router, Route} from "react-router-dom";
+import Search from "./pages/Search";
+import Saved from "./pages/Saved";
 import "./App.css";
-// import Navbar from "./components/Navbar"
+import Navbar from "./components/Navbar"
 // import Jumbotron from "./components/Jumbotron";
 // import Wrapper from "./components/Wrapper";
 
@@ -9,9 +11,10 @@ function App() {
   return (
     <>
     <Router>
+      <Navbar />
 
-
-
+      <Route exact path= "/" component={Search}></Route>
+      <Route exact path= "/saved" component={Saved}></Route>
 
 
     </Router>
