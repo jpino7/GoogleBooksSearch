09129a714e982bb18d40fa00b3696e2f7c8d4b3d
JRPino <pino3789@gmail.com>
2020-07-07 11:13:16 -0700

    Adding in components I will use, as well as setting up App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,21 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
+import React from "react";
+import {BrowserRouter as Router, Route} from "react-router-dom";
 import "./App.css";
+// import Navbar from "./components/Navbar"
+// import Jumbotron from "./components/Jumbotron";
+// import Wrapper from "./components/Wrapper";
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
-      </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
+    <>
+    <Router>
+
+
+
+
+
+    </Router>
+    </>
   );
 }
 
