8e911c088e48fc9b23be360cf870295f7f3fd483
JRPino <pino3789@gmail.com>
2020-07-06 18:03:52 -0700

    Adding in routes to server.js

--- server.js
+++ server.js
@@ -1,25 +1,62 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-const routes = require('');
-
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
 // Define API routes here
+//GET
+app.get("/api/books", (req, res) => {
+  console.log("GET ROUTE HIT!");
+  db.Book.find({})
+    .then(dbBooks => {
+      console.log(dbBooks)
+      res.json(dbBooks);
+    })
+    .catch(err => {
+      console.log(err);
+      res.json(err);
+    })
+})
+// POST
+app.post("/api/books", (req, res) => {
+  console.log("POST ROUTE HIT!");
+  console.log(req.body);
+  db.Book
+    .create(req.body)
+    .then(dbModel => { res.json(dbModel) })
+    .catch(err => res.status(422).json(err));
+});
+// DELETE
+app.delete("/api/books/:id", (req, res) => {
+  console.log("DELETE ROUTE HIT!");
+  console.log(req.params.id);
+  db.Book.deleteOne({ bookId: req.params.id }, function (err) {
+    if (err) {
+      throw err;
+    }
+    console.log("successfully deleted");
+    res.sendStatus(200);
+  })
+})
 
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
+
+// MongoDB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
