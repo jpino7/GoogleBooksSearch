759d30a49c88c06a251840a2feebaf51a6261498
JRPino <pino3789@gmail.com>
2020-07-07 12:03:39 -0700

    Added code for jumbotron, navbar, and Toast.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import {BrowserRouter as Router, Route} from "react-router-dom";
 import Search from "./pages/Search";
 import Saved from "./pages/Saved";
 import "./App.css";
-import Navbar from "./components/Navbar"
+// import Navbar from "./components/Navbar"
 // import Jumbotron from "./components/Jumbotron";
 // import Wrapper from "./components/Wrapper";
 
@@ -11,7 +11,7 @@ function App() {
   return (
     <>
     <Router>
-      <Navbar />
+      {/* <Navbar /> */}
 
       <Route exact path= "/" component={Search}></Route>
       <Route exact path= "/saved" component={Saved}></Route>
